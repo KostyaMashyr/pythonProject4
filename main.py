f3b4b4906ea88f84762208a5bd73182cf9f9e93d
KostyaMashyr <78562177+KostyaMashyr@users.noreply.github.com>
2021-02-05 10:39:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print ('git test')
+import requests
+from bs4 import BeautifulSoup
 
+url = 'https://www.olx.ua/hobbi-otdyh-i-sport/sport-otdyh/velo/velosipedy/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+items = soup.find_all('tr', class_='wrap')
+for n, i in enumerate(items, start=1):
+    itemName = i.find('h3', class_='lheight22 margintop5').text.strip()
+    itemPrice = i.find('p', class_='price').text
+    itemURL = i.find('a').get('href')
+    print(f' {itemName} {itemPrice} {itemURL}')
